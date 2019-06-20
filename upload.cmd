6fff8b9854bb30ff347078605392248c94f97001
jeremy <jeremy@bitsum.com>
2019-06-20 15:45:40 -0400

    upload script tweak

--- upload.cmd
+++ upload.cmd
@@ -1,4 +1,4 @@
-scp x64\release\corepriosetup.exe jeremy@sl3.bitsum.com:/var/www/vhosts/bitsum.com/files/corepriosetup.exe
-scp x64\release\corepriosetup.exe jeremy@sl3.bitsum.com:/var/www/vhosts/bitsum.com/files/beta/corepriosetup.exe
-scp version.txt jeremy@sl3.bitsum.com:/var/www/vhosts/bitsum.com/versioninfo/coreprio/index.php
-scp version.txt jeremy@sl3.bitsum.com:/var/www/vhosts/bitsum.com/versioninfo/coreprio/beta/index.php
+scp x64\release\corepriosetup.exe jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/corepriosetup.exe
+scp x64\release\corepriosetup.exe jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/beta/corepriosetup.exe
+scp version.txt jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/versioninfo/coreprio/index.php
+scp version.txt jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/versioninfo/coreprio/beta/index.php
